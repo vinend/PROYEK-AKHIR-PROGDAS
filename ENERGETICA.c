0d8460e5f2346ec279f9d14f3c51090fb726bd58
kimnzh <152639057+kimnzh@users.noreply.github.com>
2023-12-02 20:51:52 +0700

    Update ENERGETICA.c

--- ENERGETICA.c
+++ ENERGETICA.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
@@ -36,31 +37,29 @@ typedef struct kotaEnergetica {
 	
 } kota;
 
-
-
 int main() {
 	
-    printf(ANSI_COLOR_YELLOW "  W   W  EEEEE  L      CCCCC  OOO   M     M  EEEEE  \n");
-    printf("  W   W  E      L     C      O   O  MM   MM  E      \n");
-    printf("  W W W  EEEE   L     C      O   O  M M M M  EEEE   \n");
-    printf("  W W W  E      L     C      O   O  M  M  M  E      \n");
-    printf("   W W   EEEEE  LLLLL  CCCCC  OOO   M     M  EEEEE  \n");
+   printf(ANSI_COLOR_YELLOW" _       __________    __________  __  _________                   "ANSI_COLOR_BLUE"________\n"ANSI_COLOR_YELLOW);
+    printf("| |     / / ____/ /   / ____/ __ \\/  |/  / ____/                  "ANSI_COLOR_BLUE"/////////\n"ANSI_COLOR_YELLOW);
+    printf("| | /| / / __/ / /   / /   / / / / /|_/ / __/                    "ANSI_COLOR_BLUE"/////////\n"ANSI_COLOR_YELLOW);
+    printf("| |/ |/ / /___/ /___/ /___/ /_/ / /  / / /___                   "ANSI_COLOR_BLUE"/////////\n"ANSI_COLOR_YELLOW);
+    printf("|__/|__/_____/_____/\\____/\\____/_/  /_/_____/                  "ANSI_COLOR_BLUE"/////////___\n"ANSI_COLOR_YELLOW);
 
-    printf("\n");
+    printf("\t\t\t\t\t\t\t" "      "ANSI_COLOR_BLUE"/_____///////\n"ANSI_COLOR_YELLOW);
 
-    printf("  TTTTT  OOO   \n");
-    printf("    T   O   O  \n");
-    printf("    T   O   O  \n");
-    printf("    T   O   O  \n");
-    printf("    T    OOO   \n");
+    printf("  __________                                                       "ANSI_COLOR_BLUE"///////\n"ANSI_COLOR_YELLOW);
+    printf(" /_  __/ __ \\                                                     "ANSI_COLOR_BLUE"///////__\n"ANSI_COLOR_YELLOW);
+    printf("  / / / / / /                                                    "ANSI_COLOR_BLUE"//////////\n"ANSI_COLOR_YELLOW);
+    printf(" / / / /_/ /                                                    "ANSI_COLOR_BLUE"/____/////\n"ANSI_COLOR_YELLOW);
+    printf("/_/  \\____/                                                         "ANSI_COLOR_BLUE"/////\n"ANSI_COLOR_YELLOW);
 
-    printf("\n");
+    printf("\t\t\t\t\t\t\t\t" "   "ANSI_COLOR_BLUE"/////\n");
 
-    printf("  " ANSI_COLOR_GREEN "EEEEE" ANSI_COLOR_RESET "  N     N  " ANSI_COLOR_GREEN "EEEEE" ANSI_COLOR_RESET "  RRRR   GGG   " ANSI_COLOR_GREEN "EEEEE" ANSI_COLOR_RESET "  TTTTT  III  CCCCC  AAAAA \n");
-    printf("  " ANSI_COLOR_GREEN "E" ANSI_COLOR_RESET "      NN    N  " ANSI_COLOR_GREEN "E" ANSI_COLOR_RESET "      R   R G   G  " ANSI_COLOR_GREEN "E" ANSI_COLOR_RESET "        T     I   C      A   A \n");
-    printf("  " ANSI_COLOR_GREEN "EEEE" ANSI_COLOR_RESET "   N N   N  " ANSI_COLOR_GREEN "EEEE" ANSI_COLOR_RESET "   RRRR  G     " ANSI_COLOR_GREEN "EEEE" ANSI_COLOR_RESET "     T     I   C      AAAAA \n");
-    printf("  " ANSI_COLOR_GREEN "E" ANSI_COLOR_RESET "      N  N  N  " ANSI_COLOR_GREEN "E" ANSI_COLOR_RESET "      R  R  G G G  " ANSI_COLOR_GREEN "E" ANSI_COLOR_RESET "        T     I   C      A   A \n");
-    printf("  " ANSI_COLOR_GREEN "EEEEE" ANSI_COLOR_RESET "  N   N N  " ANSI_COLOR_GREEN "EEEEE" ANSI_COLOR_RESET "  R   R  GGG   " ANSI_COLOR_GREEN "EEEEE" ANSI_COLOR_RESET "    T    III  CCCCC  A   A \n");
+    printf(ANSI_COLOR_GREEN"    ______"ANSI_COLOR_RESET"_   __"ANSI_COLOR_GREEN"______"ANSI_COLOR_RESET"____  ______"ANSI_COLOR_GREEN"______"ANSI_COLOR_RESET"___________________\n");
+    printf(ANSI_COLOR_GREEN"   / ____/"ANSI_COLOR_RESET" | / "ANSI_COLOR_GREEN"/ ____/"ANSI_COLOR_RESET" __ \\/ ____"ANSI_COLOR_GREEN"/ ____/"ANSI_COLOR_RESET"_  __/  _/ ____/   |\n");
+    printf(ANSI_COLOR_GREEN"  / __/ "ANSI_COLOR_RESET"/  |/ "ANSI_COLOR_GREEN"/ __/ "ANSI_COLOR_RESET"/ /_/ / / __"ANSI_COLOR_GREEN"/ __/   "ANSI_COLOR_RESET"/ /  / // /   / /| |\n");
+    printf(ANSI_COLOR_GREEN" / /___"ANSI_COLOR_RESET"/ /|  "ANSI_COLOR_GREEN"/ /___"ANSI_COLOR_RESET"/ _, _/ /_/ "ANSI_COLOR_GREEN"/ /___  "ANSI_COLOR_RESET"/ / _/ // /___/ ___ |\n");
+    printf(ANSI_COLOR_GREEN"/_____/"ANSI_COLOR_RESET"_/ |_"ANSI_COLOR_GREEN"/_____/"ANSI_COLOR_RESET"_/ |_|\\____"ANSI_COLOR_GREEN"/_____/"ANSI_COLOR_RESET" /_/ /___/\\____/_/  |_|\n");
     
     printf("\n\nPress any button to Continue!");
     getch();
@@ -70,11 +69,11 @@ int main() {
     
     while(1) {
 	
-    printf("  " ANSI_COLOR_GREEN "EEEEE" ANSI_COLOR_RESET "  N     N  " ANSI_COLOR_GREEN "EEEEE" ANSI_COLOR_RESET "  RRRR   GGG   " ANSI_COLOR_GREEN "EEEEE" ANSI_COLOR_RESET "  TTTTT  III  CCCCC  AAAAA \n");
-    printf("  " ANSI_COLOR_GREEN "E" ANSI_COLOR_RESET "      NN    N  " ANSI_COLOR_GREEN "E" ANSI_COLOR_RESET "      R   R G   G  " ANSI_COLOR_GREEN "E" ANSI_COLOR_RESET "        T     I   C      A   A \n");
-    printf("  " ANSI_COLOR_GREEN "EEEE" ANSI_COLOR_RESET "   N N   N  " ANSI_COLOR_GREEN "EEEE" ANSI_COLOR_RESET "   RRRR  G      " ANSI_COLOR_GREEN "EEEE" ANSI_COLOR_RESET "     T     I   C      AAAAA \n");
-    printf("  " ANSI_COLOR_GREEN "E" ANSI_COLOR_RESET "      N  N  N  " ANSI_COLOR_GREEN "E" ANSI_COLOR_RESET "      R  R  G G G  " ANSI_COLOR_GREEN "E" ANSI_COLOR_RESET "        T     I   C      A   A \n");
-    printf("  " ANSI_COLOR_GREEN "EEEEE" ANSI_COLOR_RESET "  N   N N  " ANSI_COLOR_GREEN "EEEEE" ANSI_COLOR_RESET "  R   R  GGG   " ANSI_COLOR_GREEN "EEEEE" ANSI_COLOR_RESET "    T    III  CCCCC  A   A \n");
+    printf(ANSI_COLOR_GREEN"    ______"ANSI_COLOR_RESET"_   __"ANSI_COLOR_GREEN"______"ANSI_COLOR_RESET"____  ______"ANSI_COLOR_GREEN"______"ANSI_COLOR_RESET"___________________\n");
+    printf(ANSI_COLOR_GREEN"   / ____/"ANSI_COLOR_RESET" | / "ANSI_COLOR_GREEN"/ ____/"ANSI_COLOR_RESET" __ \\/ ____"ANSI_COLOR_GREEN"/ ____/"ANSI_COLOR_RESET"_  __/  _/ ____/   |\n");
+    printf(ANSI_COLOR_GREEN"  / __/ "ANSI_COLOR_RESET"/  |/ "ANSI_COLOR_GREEN"/ __/ "ANSI_COLOR_RESET"/ /_/ / / __"ANSI_COLOR_GREEN"/ __/   "ANSI_COLOR_RESET"/ /  / // /   / /| |\n");
+    printf(ANSI_COLOR_GREEN" / /___"ANSI_COLOR_RESET"/ /|  "ANSI_COLOR_GREEN"/ /___"ANSI_COLOR_RESET"/ _, _/ /_/ "ANSI_COLOR_GREEN"/ /___  "ANSI_COLOR_RESET"/ / _/ // /___/ ___ |\n");
+    printf(ANSI_COLOR_GREEN"/_____/"ANSI_COLOR_RESET"_/ |_"ANSI_COLOR_GREEN"/_____/"ANSI_COLOR_RESET"_/ |_|\\____"ANSI_COLOR_GREEN"/_____/"ANSI_COLOR_RESET" /_/ /___/\\____/_/  |_|\n");
     printf("\n\n");
     printf("                M   A   I   N       M   E   N   U      \n");
     printf(" " ANSI_COLOR_GREEN "========================================================================\n" ANSI_COLOR_RESET);
@@ -88,33 +87,13 @@ int main() {
     switch(pil) {
     	case 1 : system("cls"); break;
     		
-    	case 2 : 
-    	system("cls");
-    	printf("C A R A   B E R M A I N\n");
-    	printf("==============================\n");
-    	printf("1. Anda akan diberikan suatu senario dimana anda harus memecahkan masalah pada kota\n\n");
-		printf("2. Pada game ini terdapat 4 kota yang harus anda selesaikan masalahnya : Dea Tae-AE, Elegger City, Tekkompolis, dan Biotopia\n\n");
-		printf("3. Tiap kota memiliki indeks pencapaian yang meliputi 4 aspek yaitu Akses Listrik, Kebersihan Rumah Tangga, Akses Energi Bersih, dan Pengurangan Emisi Gas Rumah Kaca\n\n");
-		printf("4. Kita akan diberikan budget yang berbeda-beda setiap kota untuk memecahkan masalah-masalah ini, semua aspek yang mempengaruhi indeks harus harmonis dan saling balanced\n\n");
-		printf("5. Jika Indeks tidak tercapai, batas hari sudah habis, atau budget melebihi batas maka anda gagal\n\n");
-		printf("6. Jika Indeks tercapai dalam batas hari yang diinginkan maka anda lanjut ke level berikutnya sampai game selesai!\n\n");
-		printf("\nPress Any Button To Continue");
-		getch();
-		system("cls"); break;
+    	case 2 : system("cls"); break;
     	
-    	case 3 : 
-		system("cls");
-		
-		system("cls"); break;
+    	case 3 : system("cls"); break;
     		
-    	case 4 : return 0;
+    	case 4 : system("cls"); break;
     		
-    	default : 
-		system("cls");
-		printf("T Y P E   A   V A L I D   N U M B E R !!\n\n");
-		printf("Press Any Button To Continue");
-		getch();
-		system("cls"); break;
+    	default : system("cls"); break;
 	}
 }
     
