0c4b28e07817566808d50fe6eeb07d3b510b4752
vinend <152618406+vinend@users.noreply.github.com>
2023-12-02 16:54:51 +0700

    Pergantian Warna supaya lebih User Friendly

--- ENERGETICA.c
+++ ENERGETICA.c
@@ -1,38 +1,39 @@
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
-
-void print3DText(const char *text) {
-    // Print each character with a slight offset for a 3D effect
-    for (const char *c = text; *c != '\0'; ++c) {
-        putchar(*c);
-        putchar(' ');
-    }
-    putchar('\n');
-}
+#define ANSI_COLOR_RESET   "\x1b[0m"
+#define ANSI_COLOR_BLACK   "\x1b[30m"
+#define ANSI_COLOR_RED     "\x1b[31m"
+#define ANSI_COLOR_GREEN   "\x1b[32m"
+#define ANSI_COLOR_YELLOW  "\x1b[33m"
+#define ANSI_COLOR_BLUE    "\x1b[34m"
+#define ANSI_COLOR_MAGENTA "\x1b[35m"
+#define ANSI_COLOR_CYAN    "\x1b[36m"
+#define ANSI_COLOR_WHITE   "\x1b[37m"
 
 int main() {
-    printf("  W   W  EEEEE  L      CCCCC  OOO   M     M  EEEEE  \n");
+    printf(ANSI_COLOR_YELLOW "  W   W  EEEEE  L      CCCCC  OOO   M     M  EEEEE  \n");
     printf("  W   W  E      L     C      O   O  MM   MM  E      \n");
     printf("  W W W  EEEE   L     C      O   O  M M M M  EEEE   \n");
     printf("  W W W  E      L     C      O   O  M  M  M  E      \n");
     printf("   W W   EEEEE  LLLLL  CCCCC  OOO   M     M  EEEEE  \n");
-
+    
     printf("\n");
-
+    
     printf("  TTTTT  OOO   \n");
     printf("    T   O   O  \n");
     printf("    T   O   O  \n");
     printf("    T   O   O  \n");
     printf("    T    OOO   \n");
-
+    
     printf("\n");
 
-    printf("  EEEEE  N     N  EEEEE  RRRR   GGG   EEEEE  TTTTT  III  CCCCC  AAAAA \n");
-    printf("  E      NN    N  E      R   R G   G  E        T     I   C      A   A \n");
-    printf("  EEEE   N N   N  EEEE   RRRR  G      EEEE     T     I   C      AAAAA \n");
-    printf("  E      N  N  N  E      R  R  G   G  E        T     I   C      A   A \n");
-    printf("  EEEEE  N   N N  EEEEE  R   R  GGG   EEEEE    T    III  CCCCC  A   A \n");
+    printf("  " ANSI_COLOR_GREEN "EEEEE" ANSI_COLOR_RESET "  N     N  " ANSI_COLOR_GREEN "EEEEE" ANSI_COLOR_RESET "  RRRR   GGG   " ANSI_COLOR_GREEN "EEEEE" ANSI_COLOR_RESET "  TTTTT  III  CCCCC  AAAAA \n");
+    printf("  " ANSI_COLOR_GREEN "E" ANSI_COLOR_RESET "      NN    N  " ANSI_COLOR_GREEN "E" ANSI_COLOR_RESET "      R   R G   G  " ANSI_COLOR_GREEN "E" ANSI_COLOR_RESET "        T     I   C      A   A \n");
+    printf("  " ANSI_COLOR_GREEN "EEEE" ANSI_COLOR_RESET "   N N   N  " ANSI_COLOR_GREEN "EEEE" ANSI_COLOR_RESET "   RRRR  G      " ANSI_COLOR_GREEN "EEEE" ANSI_COLOR_RESET "     T     I   C      AAAAA \n");
+    printf("  " ANSI_COLOR_GREEN "E" ANSI_COLOR_RESET "      N  N  N  " ANSI_COLOR_GREEN "E" ANSI_COLOR_RESET "      R  R  G G G  " ANSI_COLOR_GREEN "E" ANSI_COLOR_RESET "        T     I   C      A   A \n");
+    printf("  " ANSI_COLOR_GREEN "EEEEE" ANSI_COLOR_RESET "  N   N N  " ANSI_COLOR_GREEN "EEEEE" ANSI_COLOR_RESET "  R   R  GGG   " ANSI_COLOR_GREEN "EEEEE" ANSI_COLOR_RESET "    T    III  CCCCC  A   A \n");
+    
 
     return 0;
 }
